caff3f7bd84ffa4f24d3611550165775d4d21f9c
DeceptiveHedge <monj1_14@uni.worc.ac.uk>
2017-12-04 14:44:11 +0000

    implemented parameter system

--- www/js/index.js
+++ www/js/index.js
@@ -3,41 +3,38 @@ var notification_count=0;
 $(document).on('pageinit', function() {
 
 	$('#messageButton').on('click', function() {
-		createMessage();
+		 createMessage('You are the best.', 2000);
 	});
 	
 	$('#dialogButton').on('click', function() {
-		createDialog();
+		createDialog('Are you hungry?', dialogDismissed, 'Best dialog box!', 'I am hungry', 'I am not hungry');
 	});
 
 
 	$('#notificationButton').on('click', function() {
-		createNotification();
+		createNotification(3000, "Hey you", "Get back to work.");
 	});
 
 
 });
 
-
-
-function createMessage(){		
+function createMessage(m, d){		
 	//phoneGap and jQueryMobile do not support toast messages directly
     //so we can add this using toast.js
-    new Toast({content: 'You are the best.', duration: 2000}); 
-    new Toast({content: 'No you are!', duration: 3000});
+    new Toast({content: m, duration: d}); 
 }
         	
 
-function createDialog() {
+function createDialog(m, cb, t, b1, b2) {
 
 	//phonegap supports native dialog boxes.
 	//here's a simple example
       
 	navigator.notification.confirm(
-    	'Are you hungry?',  // message
-        dialogDismissed,         // callback
-        'Best dialog box!',            // title
-        ['I am hungry', 'I am not hungry']                  // buttons
+        m,  // message
+        cb,         // callback
+        t,            // title
+        [b1, b2]                  // buttons
     );
 
 }
@@ -48,7 +45,6 @@ function dialogDismissed(buttonIndex) {
 	
 	if(buttonIndex==1) {
       new Toast({content: "Get back to work after eating.", duration: 3000}); 
-      createNotification;
     } 
    	else if(buttonIndex==2) {
       new Toast({content: 'Carry on, you can do it.', duration: 3000});
@@ -57,13 +53,13 @@ function dialogDismissed(buttonIndex) {
 
    
    
-function createNotification() {
+function createNotification(td, t, m) {
         		
 	//
     //generate a time to post notification
     //
     var currentTime = new Date().getTime(); //current time
-    var notificationTime = new Date(currentTime + 30000); //delayed time  - add 1 second
+    var notificationTime = new Date(currentTime + td); //delayed time  - add 1 second
     			
     //
     //setup notification
@@ -71,8 +67,8 @@ function createNotification() {
     
     cordova.plugins.notification.local.schedule({ 
     	id: 		1,
-        title: 		"Hey you",
-        message: 	"Get back to work.",
+        title: 		t,
+        message: 	m,
         date: 		notificationTime, 
         badge: 		notification_count++
    	});
