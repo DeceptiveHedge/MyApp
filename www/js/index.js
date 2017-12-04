feffdb108ac78692fb1e4f8920f5ee70ed77af7c
DeceptiveHedge <monj1_14@uni.worc.ac.uk>
2017-12-04 13:52:13 +0000

    Added messages

--- www/js/index.js
+++ www/js/index.js
@@ -1,27 +1,76 @@
+var notification_count=0;
 
+$(document).on('pageinit', function() {
 
-function onLoad() {
-    document.addEventListener("deviceready", onDeviceReady, false);
-    console.log("device ready");
-}
+	$('#messageButton').on('click', function() {
+		createMessage();
+	});
 	
-function updateDisplay() {
-    
+	$('#dialogButton').on('click', function() {
+		createDialog();
+	});
+
+
+	$('#notificationButton').on('click', function() {
+		createNotification();
+	});
+
+
+});
+
+
+
+function createMessage(){		
+	//phoneGap and jQueryMobile do not support toast messages directly
+    //so we can add this using toast.js
+    new Toast({content: 'You are the best.', duration: 2000}); 
+    new Toast({content: 'No you are!', duration: 3000});
+}
+        	
+
+function createDialog() {
+
+	//phonegap supports native dialog boxes.
+	//here's a simple example
+      
+	navigator.notification.confirm(
+    	'What do you think of this dialog?',  // message
+        dialogDismissed,         // callback
+        'An example dialog!',            // title
+        ['Awesome!', 'Sucks']                  // buttons
+    );
+
+}
+        	
+        	
+        	
+function dialogDismissed(buttonIndex) {
+	
+	if(buttonIndex==1) new Toast({content: "You're easily pleased", duration: 3000});
+   	else if(buttonIndex==2) new Toast({content: 'It is rather boring.', duration: 3000});
+
 }
 
-
-// device APIs are available
-//
-function onDeviceReady() {
-		
-    alert("ready to go");
-        
-    document.addEventListener("resume", onResume, false);
-    document.addEventListener("pause", onPause, false);
-		
-    console.log(device.cordova);
-		
-    launched_count++;
-    updateDisplay();
+   
+   
+function createNotification() {
+        		
+	//
+    //generate a time to post notification
+    //
+    var currentTime = new Date().getTime(); //current time
+    var notificationTime = new Date(currentTime + 1000); //delayed time  - add 1 second
+    			
+    //
+    //setup notification
+    //
+    
+    cordova.plugins.notification.local.schedule({ 
+    	id: 		1,
+        title: 		"Hey you",
+        message: 	"This is an example notification",
+        date: 		notificationTime, 
+        badge: 		notification_count++
+   	});
     
 }
