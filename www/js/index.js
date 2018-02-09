2cefae0fb9072a8cd8838f0b557ae7363bd77b5e
DeceptiveHedge <monj1_14@uni.worc.ac.uk>
2018-02-09 16:16:38 +0000

    made into server

--- www/js/index.js
+++ www/js/index.js
@@ -1,82 +1,60 @@
-var notification_count=0;
+Backendless.initApp("E81ED314-BB9B-EFD8-FF4C-74A2F7CFC800", "B3B57989-569A-3AB0-FF75-5DE0AB3FB300");
 
-$(document).on('pageinit', function() {
+$(document).on("pageshow", "#todopage", onPageShow);
 
-	$('#messageButton').on('click', function() {
-        // P1: message, P2: time on display
-		 createMessage('You are the best.', 2000);
-	});
-	
-	$('#dialogButton').on('click', function() {
-        // P1: message, P2: call-back, P3: title, P4: button1, P5: button2 
-		createDialog('Are you hungry?', dialogDismissed, 'Best dialog box!', 'I am hungry', 'I am not hungry');
-	});
+$(document).on("click", "#addTaskButton", onAddTask);
 
-
-	$('#notificationButton').on('click', function() {
-        // P1: time delay, P2: title, P3: message
-		createNotification(3000, "Hey you", "Get back to work.");
-	});
-
-
-});
-
-function createMessage(m, d){		
-	//phoneGap and jQueryMobile do not support toast messages directly
-    //so we can add this using toast.js
-    new Toast({content: m, duration: d}); 
+function onPageShow() {
+     console.log("page shown");
+    Backendless.Data.of("Task").find().then(processResults).catch(error);
+    
 }
-        	
 
-function createDialog(m, cb, t, b1, b2) {
+function onAddTask() {
+    console.log("add task button clicked");
+    
+    var tasktext = $('#TaskText').val();
+    
+    console.log(tasktext);
+    
+    var newTask = {};
+    newTask.Task = tasktext;
+    
+    Backendless.Data.of("Task").save(newTask).then(saved).catch(error);
+    Backendless.Data.of("Task").find().then(processResults).catch(error);
+}
 
-	//phonegap supports native dialog boxes.
-	//here's a simple example
-      
-	navigator.notification.confirm(
-        m,  // message
-        cb,         // callback
-        t,            // title
-        [b1, b2]                  // buttons
-    );
-
-}        	      	
-        	
-function dialogDismissed(buttonIndex) {
-	
-	if(buttonIndex==1) {
-      new Toast({content: "Get back to work after eating.", duration: 3000}); 
-    } 
-   	else if(buttonIndex==2) {
-      new Toast({content: 'Carry on, you can do it.', duration: 3000});
-      createDialog('Are you tired?', tiredDialog, 'Tired dialog box!', 'I am tired', 'I am not tired');
+function processResults(tasks) {
+    
+    console.log("processResults");
+    var taskSelect = $("#TaskSelect");
+    
+    // display the first task in an array of tasks.
+    alert(tasks[0].Task);
+    alert(tasks[1].Task);
+    
+    // wipe the list and select menu clean
+    $('#taskList').empty();
+    taskSelect.empty();
+    
+    // add each tasks to list and select menu
+    for (var i = 0; i < tasks.length; i++) {
+        $('#taskList').append("<li>"+tasks[i].Task+"</li");
+        
+        taskSelect.append("<option value='" + tasks[i]+ "'>" + tasks[i] + "</option>");
     }
+    
+    // refresh the listview
+    $('#taskList').listview('refresh');
+    
+    
 }
 
-function tiredDialog(buttonIndex){
-    if(buttonIndex==1) { new Toast({content: "Take a break, dude.", duration: 3000}); }
-    else if(buttonIndex==2) { new Toast({content: "Keep going then.", duration: 3000});}
+function saved(savedTask) {
+    console.log( "new Contact instance has been saved" + savedTask);
 }
-   
-   
-function createNotification(td, t, m) {
-        		
-	//
-    //generate a time to post notification
-    //
-    var currentTime = new Date().getTime(); //current time
-    var notificationTime = new Date(currentTime + td); //delayed time  - add 1 second
-    			
-    //
-    //setup notification
-    //
-    
-    cordova.plugins.notification.local.schedule({ 
-    	id: 		1,
-        title: 		t,
-        message: 	m,
-        date: 		notificationTime, 
-        badge: 		notification_count++
-   	});
-    
+
+function error(err) {
+    alert(err);
 }
+
